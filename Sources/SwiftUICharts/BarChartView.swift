53f2658d09f15fd9c34eaf3bcad9e6fa608db1c0
Majid Jabrayilov <cmecid@gmail.com>
2020-12-06 20:41:45 +0400

    Docs in BarChartView fixed

--- Sources/SwiftUICharts/BarChartView.swift
+++ Sources/SwiftUICharts/BarChartView.swift
@@ -26,6 +26,7 @@ public struct BarChartView: View {
         - limit: The horizontal line that will be drawn over bars. Default is nil.
         - barMinHeight: The minimal height for the bar that presents the biggest value. Default is 100.
         - showAxis: Bool value that controls whenever to show axis.
+        - axisLeadingPadding: Leading padding for axis line. Default is 0.
         - showLabels: Bool value that controls whenever to show labels.
         - labelCount: The count of labels that should be shown below the chart. Default is all.
         - showLegends: Bool value that controls whenever to show legends.
