7692dc508c7083a973483a9fdfba9a6bfa977654
Majid Jabrayilov <cmecid@gmail.com>
2020-12-08 23:52:17 +0400

    Chart style environment value introduced

---
+++ Sources/SwiftUICharts/Model/ChartStyle.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by Majid Jabrayilov on 08.12.20.
+//
+import SwiftUI
+
+protocol ChartStyle {
+    var showLabels: Bool { get }
+    var showAxis: Bool { get }
+    var showLegends: Bool { get }
+}
+
+struct ChartStyleEnvironmentKey: EnvironmentKey {
+    static var defaultValue: ChartStyle?
+}
+
+extension EnvironmentValues {
+    var chartStyle: ChartStyle? {
+        get { self[ChartStyleEnvironmentKey.self] }
+        set { self[ChartStyleEnvironmentKey.self] = newValue }
+    }
+}
+
+extension View {
+    func chartStyle(_ style: ChartStyle) -> some View {
+        environment(\.chartStyle, style)
+    }
+}
