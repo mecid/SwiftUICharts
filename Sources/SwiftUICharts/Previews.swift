fa4cb5e3050472bc836056e4919e22426303155c
Majid Jabrayilov <cmecid@gmail.com>
2022-05-31 02:19:30 +0400

    Previews file added

---
+++ Sources/SwiftUICharts/Previews.swift
@@ -0,0 +1,104 @@
+//
+//  Previews.swift
+//  SwiftUICharts
+//
+//  Created by Majid Jabrayilov on 31.05.22.
+//
+import SwiftUI
+
+#if DEBUG
+struct Previews: PreviewProvider {
+    static var previews: some View {
+        BarChartView(dataPoints: DataPoint.mock)
+            .chartStyle(
+                BarChartStyle(
+                    barMinHeight: 100,
+                    showAxis: true,
+                    axisLeadingPadding: 0,
+                    showLabels: true,
+                    labelCount: 5,
+                    showLegends: true
+                )
+            )
+        
+        BarChartView(
+            dataPoints: DataPoint.mock.map {
+                DataPoint(
+                    startValue: Double.random(in: 0..<$0.endValue),
+                    endValue: $0.endValue,
+                    label: $0.label,
+                    legend: $0.legend
+                )
+            }
+        )
+        .chartStyle(BarChartStyle(showLabels: false))
+        
+        HStack {
+            BarChartView(
+                dataPoints: Array(DataPoint.mock[0...10]),
+                limit: DataPoint.mock[0]
+            )
+            BarChartView(
+                dataPoints: Array(DataPoint.mock[11...20]),
+                limit: DataPoint.mock[1]
+            )
+        }
+        .chartStyle(BarChartStyle(showAxis: false, showLabels: false, showLegends: false))
+
+        BarChartView(dataPoints: DataPoint.mock, limit: DataPoint.mock[3])
+            .chartStyle(
+                BarChartStyle(
+                    barMinHeight: 100,
+                    showAxis: true,
+                    axisLeadingPadding: 0,
+                    showLabels: true,
+                    labelCount: 5,
+                    showLegends: true
+                )
+            )
+
+        LineChartView(dataPoints: DataPoint.mock)
+            .chartStyle(
+                LineChartStyle(
+                    lineMinHeight: 100,
+                    showAxis: true,
+                    axisLeadingPadding: 0,
+                    showLabels: true,
+                    labelCount: 10,
+                    showLegends: true
+                )
+            )
+        
+        StackedHorizontalBarChartView(
+            dataPoints: Array(DataPoint.mock[0...7])
+        )
+        .frame(maxHeight: 80)
+        .padding()
+        .chartStyle(
+            StackedHorizontalBarChartStyle(
+                showLegends: true,
+                cornerRadius: 8,
+                spacing: 0
+            )
+        )
+        
+        HorizontalBarChartView(
+            dataPoints: DataPoint.mock,
+            barMaxWidth: 200
+        )
+        
+        LineChartView(dataPoints: DataPoint.mock)
+            .chartStyle(
+                LineChartStyle(
+                    lineMinHeight: 100,
+                    showAxis: true,
+                    axisLeadingPadding: 0,
+                    showLabels: true,
+                    labelCount: 10,
+                    showLegends: true
+                )
+            )
+        
+    }
+}
+#endif
